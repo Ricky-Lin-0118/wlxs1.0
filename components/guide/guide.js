198407ed4a272591ffd14a034d41d41212458fa4
Ricky-Lin-0118 <1258503902@qq.com>
2024-04-25 09:27:24 +0800

    1

--- components/guide/guide.js
+++ components/guide/guide.js
@@ -1,26 +1,87 @@
 Component({
   data: {
-    imageUrl: '',
-    textInfo: '',
-    message: '',
-
-  },
-  onLoad(){
-    wx.request({
-      url: '',
-      success: (res) => {
-        const imageUrl = res.data.imageUrl;
-        const textInfo = rex.data.textInfo;
-        const message = rex.data.message;
-        this.setData({
-          imageUrl: imageUrl,
-          textInfo: textInfo,
-          message: message
-        })
+    dataList: [{
+        img: 'http://pic.funworld.cc/pic/1.png',
+        tag: '#情感倾诉',
+        viewers: '223',
+        num: '2233',
+        commit: [{
+            avatar: 'http://pic.funworld.cc/pic/狗.png',
+            user: '只要ta',
+            content: '倾听是交流的基础，只有认真倾听他人的发言，我们才能理解他人的想法和需求',
+          },
+          {
+            avatar: 'http://pic.funworld.cc/pic/狗.png',
+            user: '只要ta',
+            content: '倾听是交流的基础，只有认真倾听他人的发言，我们才能理解他人的想法和需求',
+          },
+          {
+            avatar: 'http://pic.funworld.cc/pic/狗.png',
+            user: '只要ta',
+            content: '倾听是交流的基础，只有认真倾听他人的发言，我们才能理解他人的想法和需求',
+          },
+        ]
       },
-      fail: (err) => {
-        console.error(err);
+      {
+        img: 'http://pic.funworld.cc/pic/1.png',
+        tag: '#情感倾诉',
+        viewers: '223',
+        num: '2233',
+        commit: [{
+            avatar: 'http://pic.funworld.cc/pic/狗.png',
+            user: '只要ta',
+            content: '倾听是交流的基础，只有认真倾听他人的发言，我们才能理解他人的想法和需求',
+          },
+          {
+            avatar: 'http://pic.funworld.cc/pic/狗.png',
+            user: '只要ta',
+            content: '倾听是交流的基础，只有认真倾听他人的发言，我们才能理解他人的想法和需求',
+          },
+          {
+            avatar: 'http://pic.funworld.cc/pic/狗.png',
+            user: '只要ta',
+            content: '倾听是交流的基础，只有认真倾听他人的发言，我们才能理解他人的想法和需求',
+          },
+        ]
+      },
+      {
+        img: 'http://pic.funworld.cc/pic/1.png',
+        tag: '#情感倾诉',
+        viewers: '223',
+        num: '2233',
+        commit: [{
+            avatar: 'http://pic.funworld.cc/pic/狗.png',
+            user: '只要ta',
+            content: '倾听是交流的基础，只有认真倾听他人的发言，我们才能理解他人的想法和需求',
+          },
+          {
+            avatar: 'http://pic.funworld.cc/pic/狗.png',
+            user: '只要ta',
+            content: '倾听是交流的基础，只有认真倾听他人的发言，我们才能理解他人的想法和需求',
+          },
+          {
+            avatar: 'http://pic.funworld.cc/pic/狗.png',
+            user: '只要ta',
+            content: '倾听是交流的基础，只有认真倾听他人的发言，我们才能理解他人的想法和需求',
+          },
+        ]
       }
-    })
+    ]
+  },
+  methods: {
+    handleInput: function () {
+      wx.request({
+        url: '',
+        method: 'GET',
+        success: (res) => {
+          this.setData({
+            dataList: res.data
+          });
+        },
+        fail: (err) => {
+          console.error(err);
+        }
+      })
+    }
   }
 })
--- components/recommend/recommend.js
+++ components/recommend/recommend.js
@@ -1,21 +1,55 @@
 Component({
   data: {
+    properties: {
+      // 是否已经点赞的状态
+      isLiked: {
+        type: Boolean,
+        value: false
+      }
+    },
+  
     dataList: [
       {
         index: 1,
-        avatarUrl: '../../image/白榆.png',
+        avatarUrl: 'http://pic.funworld.cc/pic/白榆.png',
         imageList: [
           {
-            url: '../../image/1.png'
+            url: 'http://pic.funworld.cc/pic/知识1.png'
           },
           {
-            url: '../../image/1.png'
+            url: 'http://pic.funworld.cc/pic/知识2.png'
           },
           {
-            url: '../../image/1.png'
+            url: 'http://pic.funworld.cc/pic/知识3.png'
+          },
+          // {
+          //   url: '../../image/1.png'
+          // }
+        ],
+        name: '白榆',
+        time: '32分钟前',
+        title: '学习交流——共筑知识之桥',
+        content: '学习，是每个人成长道路上不可或缺的一部分。在这个信息爆炸的时代，如何高效学习...',
+        tag: '#学习交流',
+        viewers: '233',
+        likeCount: '45',
+        commentCount: '12'
+      },
+      {
+        index: 1,
+        avatarUrl: 'http://pic.funworld.cc/pic/白榆.png',
+        imageList: [
+          {
+            url: 'http://pic.funworld.cc/pic/1.png'
           },
           {
-            url: '../../image/1.png'
+            url: 'http://pic.funworld.cc/pic/1.png'
+          },
+          {
+            url: 'http://pic.funworld.cc/pic/1.png'
+          },
+          {
+            url: 'http://pic.funworld.cc/pic/1.png'
           }
         ],
         name: '白榆',
@@ -29,45 +63,19 @@ Component({
       },
       {
         index: 1,
-        avatarUrl: '../../image/白榆.png',
+        avatarUrl: 'http://pic.funworld.cc/pic/白榆.png',
         imageList: [
           {
-            url: '../../image/1.png'
+            url: 'http://pic.funworld.cc/pic/1.png'
           },
           {
-            url: '../../image/1.png'
+            url: 'http://pic.funworld.cc/pic/1.png'
           },
           {
-            url: '../../image/1.png'
+            url: 'http://pic.funworld.cc/pic/1.png'
           },
           {
-            url: '../../image/1.png'
-          }
-        ],
-        name: '白榆',
-        time: '32分钟前',
-        title: '学习交流——共筑知识之桥',
-        content: '学习，是每个人成长道路上不可或缺的一部分。在这个信息爆炸的时代，如何高效学习...',
-        tag: '#学习交流',
-        viewers: '233',
-        likeCount: '45',
-        commentCount: '12'
-      },
-      {
-        index: 1,
-        avatarUrl: '../../image/白榆.png',
-        imageList: [
-          {
-            url: '../../image/1.png'
-          },
-          {
-            url: '../../image/1.png'
-          },
-          {
-            url: '../../image/1.png'
-          },
-          {
-            url: '../../image/1.png'
+            url: 'http://pic.funworld.cc/pic/1.png'
           }
         ],
         name: '白榆',
@@ -95,6 +103,13 @@ Component({
           console.error(err);
         }
       })
+    },
+    toggleLike: function() {
+      this.setData({
+        isLiked: !this.data.isLiked
+      });
+      // 触发自定义事件，通知父组件状态变化
+      this.triggerEvent('like', { isLiked: this.data.isLiked });
     }
   }
 })
